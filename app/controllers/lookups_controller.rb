8a10b46f24bd280727f81e4831ff0b60ac2eada8
Nick Schneble <nschneble@users.noreply.github.com>
2024-01-24 15:13:31 -0500

    allows lookups to be created using weather api data

--- app/controllers/lookups_controller.rb
+++ app/controllers/lookups_controller.rb
@@ -1,5 +1,7 @@
 class LookupsController < ApplicationController
   before_action :set_lookup, only: %i[ show edit update destroy ]
+  before_action :set_lookup_from_query, only: %i[ create ]
+  before_action :query_weather_api, only: %i[ create ]
 
   # GET /lookups or /lookups.json
   def index
@@ -21,15 +23,23 @@ class LookupsController < ApplicationController
 
   # POST /lookups or /lookups.json
   def create
-    @lookup = Lookup.new(lookup_params)
+    # checks if we already have a recently cached lookup to display
+    if @lookup.present?
+      respond_to do |format|
+        format.html { redirect_to lookup_url(@lookup), notice: "These weather conditions were loaded from a cache from #{helpers.time_ago_in_words(@lookup.cached_at)} ago" }
+        format.json { render :show, status: :ok, location: @lookup }
+      end
+    else
+      @lookup = Lookup.new(lookup_params)
 
-    respond_to do |format|
-      if @lookup.save
-        format.html { redirect_to lookup_url(@lookup), notice: "Lookup was successfully created." }
-        format.json { render :show, status: :created, location: @lookup }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @lookup.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        if @lookup.save
+          format.html { redirect_to lookup_url(@lookup) }
+          format.json { render :show, status: :created, location: @lookup }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @lookup.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
@@ -63,8 +73,26 @@ class LookupsController < ApplicationController
       @lookup = Lookup.find(params[:id])
     end
 
+    def set_lookup_from_query
+      query = params[:query]
+      scale = params[:scale]
+
+      @lookup = Lookup.find_by("zip_code = ? AND scale = ? AND cached_at > ?", query, scale, 30.minutes.ago)
+    end
+
+    # Uses WeatherAPI.com to lookup weather based on the search query
+    def query_weather_api
+      # skips if we already have a recently cached lookup to display
+      unless @lookup.present?
+        query = params[:query]
+        scale = params[:scale]
+
+        params[:lookup] = helpers.supply_lookup_params_from_weather_query(query, scale)
+      end
+    end
+
     # Only allow a list of trusted parameters through.
     def lookup_params
-      params.require(:lookup).permit(:zip_code, :cached_at, :response)
+      params.require(:lookup).permit(:zip_code, :cached_at, :scale, :temperature, :high, :low, :condition, :icon)
     end
 end
