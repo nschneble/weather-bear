1e9aad612d7fb4925f78e0f3a170980c893f82ea
Nick Schneble <nschneble@users.noreply.github.com>
2024-01-24 11:40:58 -0500

    remove the "click here" from the joke

--- app/javascript/controllers/joke_controller.js
+++ app/javascript/controllers/joke_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 	connect() {
-		this.element.textContent = "\"What do you call a bear caught in the rain?\" (click)"
+		this.element.textContent = "\"What do you call a bear caught in the rain?\""
 	}
 
 	handleClick() {
