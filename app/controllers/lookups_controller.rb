ecdce0b462577a625a72ac637b8d42b5da2e4c51
Nick Schneble <nschneble@users.noreply.github.com>
2024-01-24 17:21:27 -0500

    add helper to use geocoding to try and find zip codes

--- app/controllers/lookups_controller.rb
+++ app/controllers/lookups_controller.rb
@@ -1,6 +1,7 @@
 class LookupsController < ApplicationController
   before_action :set_lookup, only: %i[ show edit update destroy ]
-  before_action :set_lookup_from_query, only: %i[ create ]
+  before_action :set_zip_code_from_query, only: %i[ create ]
+  before_action :set_lookup_from_zip_code, only: %i[ create ]
   before_action :query_weather_api, only: %i[ create ]
 
   # GET /lookups or /lookups.json
@@ -73,21 +74,36 @@ class LookupsController < ApplicationController
       @lookup = Lookup.find(params[:id])
     end
 
-    def set_lookup_from_query
+    def set_zip_code_from_query
       query = params[:query]
+
+      unless helpers.query_is_zip_code(query)
+        params[:query] = helpers.lookup_zip_code_params_from_query(query)
+      end
+
+      unless helpers.query_is_zip_code(params[:query])
+        respond_to do |format|
+          format.html { redirect_to root_path, notice: "Sorry, but we couldn't make sense of that address. Try again?" }
+          format.json { render json: @lookup.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def set_lookup_from_zip_code
+      zip_code = params[:query]
       scale = params[:scale]
 
-      @lookup = Lookup.find_by("zip_code = ? AND scale = ? AND cached_at > ?", query, scale, 30.minutes.ago)
+      @lookup = Lookup.find_by("zip_code = ? AND scale = ? AND cached_at > ?", zip_code, scale, 30.minutes.ago)
     end
 
     # Uses WeatherAPI.com to lookup weather based on the search query
     def query_weather_api
       # skips if we already have a recently cached lookup to display
       unless @lookup.present?
-        query = params[:query]
+        zip_code = params[:query]
         scale = params[:scale]
 
-        params[:lookup] = helpers.supply_lookup_params_from_weather_query(query, scale)
+        params[:lookup] = helpers.supply_lookup_params_from_weather_query(zip_code, scale)
       end
     end
 
--- app/helpers/lookups_helper.rb
+++ app/helpers/lookups_helper.rb
@@ -1,20 +1,47 @@
 module LookupsHelper
+	GEOCODING_API_BASE_URL = 'https://geocode.maps.co/search?'.freeze
+	GEOCODING_API_KEY = Figaro.env.geocoding_api_key.freeze
+
 	WEATHER_API_BASE_URL = 'https://api.weatherapi.com/v1/forecast.json?'.freeze
 	WEATHER_API_KEY = Figaro.env.weather_api_key.freeze
 
-	def supply_lookup_params_from_weather_query(query, scale)
-		url = "#{WEATHER_API_BASE_URL}key=#{WEATHER_API_KEY}&q=#{query}&days=7&aqi=no&alerts=no"
+	GEOCODING_EXTRACT_ZIP_CODE_REGEX = /^.+, (?<zip_code>\d{5}), United States$/
+	ZIP_CODE_REGEX = /^\d{5}$/
+
+	def query_is_zip_code(query)
+		ZIP_CODE_REGEX.match(query).present?
+	end
+
+	def lookup_zip_code_params_from_query(query)
+		# can't do anything, so just return the OG query and hope for the best!
+		return query unless Figaro.env.geocoding_api_key?
+
+		# let's goooooooooooo
+		url = "#{GEOCODING_API_BASE_URL}api_key=#{GEOCODING_API_KEY}&q=#{query}"
+		response = HTTParty.get(url)
+
+		return query unless response.code == 200
+
+		data = JSON.parse(response.body)
+
+		match_data = GEOCODING_EXTRACT_ZIP_CODE_REGEX.match(data[0]['display_name'])
+		return match_data['zip_code'] if match_data.present? && query_is_zip_code(match_data['zip_code'])
+
+		query
+	end
+
+	def supply_lookup_params_from_weather_query(zip_code, scale)
+		url = "#{WEATHER_API_BASE_URL}key=#{WEATHER_API_KEY}&q=#{zip_code}&days=7&aqi=no&alerts=no"
 		response = HTTParty.get(url)
 
 		return {} unless response.code == 200
 
 		params = {}
-		params[:zip_code] = query
+		params[:zip_code] = zip_code
 		params[:cached_at] = Time.now.to_s
 		params[:scale] = scale
 
 		data = JSON.parse(response.body)
-		current = data['current']
 
 		# checks if we should store results in Fahrenheit or Celsius
 		if scale.eql? 'fahrenheit'
